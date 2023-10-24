0ecd7595b52db374127e2035a04953351b749770
ARUN KUMAR SAHA <108371851+Arunkrsaha@users.noreply.github.com>
2023-10-24 09:52:02 +0530

    Added solution - LeetHub

--- Bipartite Graph - GFG/bipartite-graph.cpp
+++ Bipartite Graph - GFG/bipartite-graph.cpp
@@ -4,48 +4,33 @@ using namespace std;
 
 // } Driver Code Ends
 class Solution {
-private:
-    // Colors a component
-    bool check(int start, int V, vector<int>adj[], int color[])
+public:
+    bool dfs(int node, int col, int color[], vector<int> adj[])
     {
-        queue<int> q;
-        q.push(start);
-        color[start]=0;
-        while(!q.empty())
+        color[node]=col;
+        for(auto it : adj[node])
         {
-            int node= q.front();
-            q.pop();
-            for(auto it : adj[node]) {
-	            // if the adjacent node is yet not colored
-	            // you will give the opposite color of the node 
-	            if(color[it] == -1) {
-	                
-	                color[it] = !color[node]; 
-	                q.push(it); 
-	            }
-	            // is the adjacent guy having the same color 
-	            // someone did color it on some other path 
-	            else if(color[it] == color[node]) {
-	                return false; 
-	            }
-	        }
+            if(color[it]==-1)
+            {
+                if(dfs(it,!col,color,adj)==false) return false;
+            }
+            else if(color[it] == col)
+            {
+                return false;
+            }
         }
         return true;
     }
-public:
 	bool isBipartite(int V, vector<int>adj[]){
-	    // Code here
+	    
 	    int color[V];
 	    for(int i=0;i<V;i++) color[i]=-1;
+	    
 	    for(int i=0;i<V;i++)
 	    {
-	        // If not colored
 	        if(color[i]==-1)
 	        {
-	            if(check(i,V,adj,color) == false)
-	            {
-	                return false;
-	            }
+	            if(dfs(i,0,color,adj)==false) return false;
 	        }
 	    }
 	    return true;
