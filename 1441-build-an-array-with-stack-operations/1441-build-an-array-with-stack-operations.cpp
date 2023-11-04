219636dfcae98591e522b6f1963be3ded0f11573
ARUN KUMAR SAHA <108371851+Arunkrsaha@users.noreply.github.com>
2023-11-04 21:48:49 +0530

    Time: 0 ms (100.00%), Space: 8.1 MB (76.67%) - LeetHub

--- 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.cpp
+++ 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.cpp
@@ -1,17 +1,22 @@
 class Solution {
 public:
     vector<string> buildArray(vector<int>& target, int n) {
-        vector<string> output;
-        int should_ve = 1;
-        for(int i=1;i<=target.size();i++){
-            if(target[i-1] != should_ve){
-                int cnt = target[i-1]-should_ve;
-                while(cnt--)
-                    output.push_back("Push"), output.push_back("Pop");
+        int stream = 1;
+        
+        vector<string> result;
+        
+        int i = 0;
+        while(i < target.size() && stream <= n) {
+            result.push_back("Push");
+            
+            if(stream == target[i]) {
+                i++;
+            } else {
+                result.push_back("Pop");
             }
-            output.push_back("Push");
-            should_ve = target[i-1] + 1;
+            stream++;
         }
-        return output;
+        
+        return result;
     }
 };
