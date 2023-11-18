5be067262988fbf6695114f82b6171ea3065d2d2
ARUN KUMAR SAHA <108371851+Arunkrsaha@users.noreply.github.com>
2023-11-18 20:22:45 +0530

    Time: 217 ms (11.76%), Space: 100.2 MB (8.19%) - LeetHub

--- 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.cpp
+++ 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.cpp
@@ -1,18 +1,31 @@
 class Solution {
 public:
     int maxFrequency(vector<int>& nums, int k) {
-        int ans = 0;
-        long sum = 0;
-
-        ranges::sort(nums);
-
-        for (int l = 0, r = 0; r < nums.size(); ++r) {
-          sum += nums[r];
-          while (sum + k < static_cast<long>(nums[r]) * (r - l + 1))
-            sum -= nums[l++];
-          ans = max(ans, r - l + 1);
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        long long s[n + 1];
+        s[0] = 0;
+        for (int i = 0; i < n; ++i) {
+            s[i + 1] = s[i] + nums[i];
         }
-
-        return ans;
+        int left = 1, right = n;
+        auto check = [&](int cnt) {
+            for (int i = 0; i < n + 1 - cnt; ++i) {
+                int j = i + cnt - 1;
+                if (1LL * nums[j] * cnt - (s[j + 1] - s[i]) <= k) {
+                    return true;
+                }
+            }
+            return false;
+        };
+        while (left < right) {
+            int mid = (left + right + 1) >> 1;
+            if (check(mid)) {
+                left = mid;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return left;
     }
 };
