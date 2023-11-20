2003d3984991788de5fc351ead92ccbcdb368b2d
ARUN KUMAR SAHA <108371851+Arunkrsaha@users.noreply.github.com>
2023-11-20 23:03:59 +0530

    Time: 157 ms (86.83%), Space: 102.4 MB (58.71%) - LeetHub

--- 2391-minimum-amount-of-time-to-collect-garbage/2391-minimum-amount-of-time-to-collect-garbage.cpp
+++ 2391-minimum-amount-of-time-to-collect-garbage/2391-minimum-amount-of-time-to-collect-garbage.cpp
@@ -1,25 +1,17 @@
 class Solution {
-private:
-  int getTime(const vector<string>& garbage, const vector<int>& prefix,
-              char c) {
-    int characterCount = 0;
-    int lastIndex = -1;
-    for (int i = 0; i < garbage.size(); ++i) {
-      const string& s = garbage[i];
-      if (ranges::any_of(s, [c](const char g) { return g == c; }))
-        lastIndex = i;
-      characterCount += std::ranges::count(s, c);
-    }
-    return characterCount + (lastIndex <= 0 ? 0 : prefix[lastIndex - 1]);
-  }
-    
 public:
     int garbageCollection(vector<string>& garbage, vector<int>& travel) {
-        vector<int> prefix(travel.size());
-        partial_sum(travel.begin(), travel.end(), prefix.begin());
-        const int timeM = getTime(garbage, prefix, 'M');
-        const int timeP = getTime(garbage, prefix, 'P');
-        const int timeG = getTime(garbage, prefix, 'G');
-        return timeM + timeP + timeG;
+        int last[128] = {}, res = 0;
+        for (int i = 0; i < garbage.size(); ++i) {
+            res += garbage[i].size();
+            for (char c : garbage[i])
+                last[c] = i;
+        }
+        for (int j = 1; j < travel.size(); ++j)
+            travel[j] += travel[j - 1];
+        for (int c : "PGM")
+            if (last[c])
+                res += travel[last[c] - 1];
+        return res;
     }
 };
