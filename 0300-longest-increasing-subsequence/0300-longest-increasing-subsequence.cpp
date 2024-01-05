fa121e00f33fe2538ce017558d42759cd18113bd
ARUN KUMAR SAHA <108371851+Arunkrsaha@users.noreply.github.com>
2024-01-05 20:02:49 +0530

    Time: 2 ms (98.77%), Space: 10.8 MB (80.76%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,31 +1,21 @@
 class Solution {
 public:
-    int n;
-    int t[2501][2501];
-    int solve(int curr, int prev, vector<int> &nums)
-    {
-        if(curr>=n)
-        {
-            return 0;
-        }
-        if(prev != -1 && t[curr][prev] != -1)
-        {
-            return t[curr][prev];
-        }
-        int take=0;
-        if(prev==-1 || nums[curr]>nums[prev])
-        {
-            take= 1+solve(curr+1,curr, nums);
-        }
-        int skip= solve(curr+1, prev, nums);
-        if(prev!=-1)
-            t[curr][prev]=max(take, skip);
-        
-        return max(take, skip);
-    }
     int lengthOfLIS(vector<int>& nums) {
-        n=nums.size();
-        memset(t,-1,sizeof(t));
-        return solve(0,-1, nums);
+        int n= nums.size();
+        vector<int> ans;
+        for(int i=0;i<n;i++)
+        {
+            auto it = lower_bound(begin(ans), end(ans), nums[i]);
+            
+            if(it==end(ans))
+            {
+                ans.push_back(nums[i]);
+            }
+            else
+            {
+                *it=nums[i];
+            }
+        }
+        return ans.size();
     }
 };
