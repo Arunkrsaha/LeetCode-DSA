fde54e97f65ca00e2fa13ec79e85ca7ce22dabc3
ARUN KUMAR SAHA <108371851+Arunkrsaha@users.noreply.github.com>
2024-01-07 00:04:22 +0530

    Time: 170 ms (69.02%), Space: 75.7 MB (59.96%) - LeetHub

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.cpp
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.cpp
@@ -1,51 +1,63 @@
-struct Job {
-  int startTime;
-  int endTime;
-  int profit;
-  Job(int startTime, int endTime, int profit)
-      : startTime(startTime), endTime(endTime), profit(profit) {}
-};
-
 class Solution {
- public:
-  int jobScheduling(vector<int>& startTime, vector<int>& endTime,
-                    vector<int>& profit) {
-    const int n = startTime.size();
-    // dp[i] := the maximum profit to schedule jobs[i..n)
-    dp.resize(n + 1);
-    vector<Job> jobs;
-
-    for (int i = 0; i < n; ++i)
-      jobs.emplace_back(startTime[i], endTime[i], profit[i]);
-
-    ranges::sort(jobs, [](const auto& a, const auto& b) {
-      return a.startTime < b.startTime;
-    });
-
-    // Will use binary search to find the first available startTime
-    for (int i = 0; i < n; ++i)
-      startTime[i] = jobs[i].startTime;
-
-    return jobScheduling(jobs, startTime, 0);
-  }
-
- private:
-  vector<int> dp;
-
-  int jobScheduling(const vector<Job>& jobs, const vector<int>& startTime,
-                    int i) {
-    if (i == jobs.size())
-      return 0;
-    if (dp[i] > 0)
-      return dp[i];
-
-    const int j = firstGreaterEqual(startTime, i + 1, jobs[i].endTime);
-    const int pick = jobs[i].profit + jobScheduling(jobs, startTime, j);
-    const int skip = jobScheduling(jobs, startTime, i + 1);
-    return dp[i] = max(pick, skip);
-  }
-
-  int firstGreaterEqual(const vector<int>& A, int startFrom, int target) {
-    return lower_bound(A.begin() + startFrom, A.end(), target) - A.begin();
-  }
+public:
+    int n;
+    int memo[50001];
+    
+    int getNext(vector<vector<int>> &array, int left, int currentJobEnd)
+    {
+        int right = n-1;
+        int result = n+1;
+        while(left<=right)
+        {
+            int mid = left+ (right-left)/2;
+            if(array[mid][0]>=currentJobEnd)
+            {
+                result = mid;
+                right = mid-1;
+            }
+            else
+            {
+                left= mid+1;
+            }
+        }
+        return result;
+    }
+    
+    int solve(vector<vector<int>> & array, int i)
+    {
+        if(i>=n)
+        {
+            return 0;
+        }
+        if(memo[i] != -1)
+            return memo[i];
+        int next = getNext(array, i+1, array[i][1]);
+        
+        int take = array[i][2] + solve(array, next);
+        int notTake = solve(array, i+1);
+        
+        return memo[i] = max(take, notTake);
+    }
+    
+    int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) {
+        n= startTime.size();
+        memset(memo, -1, sizeof(memo));
+        vector<vector<int>> array(n, vector<int> (3,0)); // {start, end, profit}
+        
+        for(int i=0;i<n;i++)
+        {
+            array[i][0]= startTime[i];
+            array[i][1]= endTime[i];
+            array[i][2]= profit[i];
+        }
+        
+        // Required to sort on yhe basis of staarting time
+        auto comp = [&](auto& vec1, auto& vec2)
+        {
+            return vec1[0] <= vec2[0];
+        };
+        sort(begin(array), end(array), comp);
+        
+        return solve(array,0);
+    }
 };
