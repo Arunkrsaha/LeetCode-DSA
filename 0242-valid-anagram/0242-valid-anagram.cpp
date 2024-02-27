1128856bb678f40c57e095ec393fa1da314964fd
ARUN KUMAR SAHA <108371851+Arunkrsaha@users.noreply.github.com>
2024-02-27 21:22:02 +0530

    Time: 12 ms (33.47%), Space: 9.7 MB (11.82%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,20 +1,32 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if (s.length() != t.length())
-      return false;
-
-    vector<int> count(26);
-
-    for (const char c : s)
-      ++count[c - 'a'];
-
-    for (const char c : t) {
-      if (count[c - 'a'] == 0)
-        return false;
-      --count[c - 'a'];
-    }
-
-    return true;
+        int n1 = s.length();
+        int n2 = t.length();
+        
+        if(n1 != n2)
+        {
+            return false;
+        }
+        
+        unordered_map<char, int> smap;
+        
+        for(int i=0;i<n1;i++)
+        {
+            smap[s[i]]++;
+        }
+        
+        for(int i=0;i<n2;i++)
+        {
+            if(smap[t[i]]<=0)
+            {
+                return false;
+            }
+            else
+            {
+                smap[t[i]]--;
+            }
+        }
+        return true;
     }
 };
